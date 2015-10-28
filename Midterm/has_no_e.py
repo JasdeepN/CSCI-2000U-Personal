5267fca742ce52d4a67f310df3530e00fba705f8
JasdeepN <jasdeep4@hotmail.com>
2015-10-28 09:33:38 -0400

    Midterm: completed has_no_e.py (as much as possible)

---
+++ Midterm/has_no_e.py
@@ -0,0 +1,16 @@
+#python
+def has_no_e(input_string):
+	for x in input_string:
+		pass
+		if (x == 'e'):
+			print ('true')
+			return
+
+	print('false')
+		
+
+
+has_no_e(raw_input()) #terminal input 
+
+
+ 	
