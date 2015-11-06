97f037063862fedbace8347064e4236d67019e82
JasdeepN <jasdeepn4@hotmail.com>
2015-11-06 13:15:22 -0500

    now outputs to terminal

--- Assignment-2/strip.sh
+++ Assignment-2/strip.sh
@@ -6,18 +6,7 @@ read k #number of lines to remove from top
 read m #number of lines to remove from bottom
 read filename #name of file
 
-#cat $filename | head -n $k #returns top k lines
-#cat $filename | tail -n $m #returns bottom m lines
-#echo "Printing >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" $filename
-#cat $filename
 
-#cat $filename | head -n $k 
-
-#cat $filename
-                 # Integer.
-let "k += 1"
-echo "k = $k "           # a = 2335         
-tail -n +$k $filename | head -n -$m | cat > "temp_file_1.txt"
-#head -n -$m "temp_file_1.txt"
-#want top 26 lines and bottom 2 lines removed everything else stays
-#TAIL | HEAD | cat 
+let "k += 1" #add 1 to k so tail doesnt go out of bounds
+            
+echo | tail -n +$k $filename | head -n -$m  #runs input->tail->output->head->output->terminal
