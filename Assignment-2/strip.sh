a18209ef293362bdb145ae8806a2c093239f2326
JasdeepN <jasdeepn4@hotmail.com>
2015-11-06 12:57:58 -0500

    removes top k lines from input file, saves all other data in new temp file

---
+++ Assignment-2/strip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Jasdeep Nijjar - 100493157
+
+#WANT THIS PROGRAM TO DO THE FOLLOWING: REOMVE K LINES FROM TOP AND M LINES FROM BOTTOM PRINT TO TERMINAL
+read k #number of lines to remove from top
+read m #number of lines to remove from bottom
+read filename #name of file
+
+#cat $filename | head -n $k #returns top k lines
+#cat $filename | tail -n $m #returns bottom m lines
+#echo "Printing >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" $filename
+#cat $filename
+
+#cat $filename | head -n $k 
+
+#cat $filename
+                 # Integer.
+let "k += 1"
+echo "k = $k "           # a = 2335         
+tail -n +$k $filename | cat > "temp_file_1.txt"
+#want top 26 lines and bottom 2 lines removed everything else stays
+#TAIL | HEAD | cat 
