266fdfbaf2c2bc3edc57aab90e3501086a2be8ca
JasdeepN <jasdeepn4@hotmail.com>
2015-11-06 13:28:15 -0500

    Assignment 2: completed strip.sh

--- Assignment-2/strip.sh
+++ Assignment-2/strip.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #Jasdeep Nijjar - 100493157
 
-#WANT THIS PROGRAM TO DO THE FOLLOWING: REOMVE K LINES FROM TOP AND M LINES FROM BOTTOM PRINT TO TERMINAL
+#removes k lines from top and m lines from bottom of input file use > to output to file
 read k #number of lines to remove from top
 read m #number of lines to remove from bottom
 read filename #name of file
 
 
 let "k += 1" #add 1 to k so tail doesnt go out of bounds
-            
+
 echo | tail -n +$k $filename | head -n -$m  #runs input->tail->output->head->output->terminal
