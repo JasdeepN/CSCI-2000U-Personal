8e54ec386a95f601f5e46cb4bc7e8fe70298d26b
JasdeepN <jasdeep4@hotmail.com>
2015-11-06 22:43:09 -0500

    takes input from terminal checks for letter 'e' in input string, returns true if 'e' is found false otherwise

---
+++ Assignment-2/has_no_e.py
@@ -0,0 +1,14 @@
+#python
+#finds weather file contains the letter e returns true if it finds the letter 'e'
+def has_no_e(input_string):
+	for x in input_string:
+		pass
+		if (x == 'e'):
+			print ('true')
+			return
+
+	print('false')
+		
+
+
+has_no_e(raw_input()) #terminal input 
