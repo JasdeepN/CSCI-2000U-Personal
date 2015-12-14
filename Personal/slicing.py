fbe1b781c5f3f113152e3063e6876412a075e07b
JasdeepN <jasdeepn4@hotmail.com>
2015-12-14 01:54:23 -0500

    adding examples

---
+++ Personal/slicing.py
@@ -0,0 +1,4 @@
+element = 'uranium'
+print("should be RAN: is " , element[1:4], "from index a to index b, [a,b)")
+print("should be URAN: is " , element[:4], "from blank to index a, [start of list:a)")
+print("should be IUM: is " , element[4:], "from index a to blank, [a:end of list]")
