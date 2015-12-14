c68a8b5b39b348681d81cad04db35c39cc2ab4ec
JasdeepN <jasdeep4@hotmail.com>
2015-12-14 17:00:31 -0500

    adding examples

---
+++ Personal/raw_input.py
@@ -0,0 +1,4 @@
+#python 
+
+name = raw_input ("what is your name?\n")
+print "your name is", name
--- Personal/slicing.py
+++ Personal/slicing.py
@@ -2,3 +2,8 @@ element = 'uranium'
 print("should be RAN: is " , element[1:4], "from index a to index b, [a,b)")
 print("should be URAN: is " , element[:4], "from blank to index a, [start of list:a)")
 print("should be IUM: is " , element[4:], "from index a to blank, [a:end of list]")
+print(element[1:1])
+print(element[3:1])
+print(element[2:1])
+print(element[1:-1])
+print(element[-7:-2])
